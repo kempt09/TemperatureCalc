fd877479b715b1c51ea3127b40ede048bffc4c4f
christopher <kempt09@gmail.com>
2016-02-04 14:15:13 -0500

    Refactor functions and HTML

--- app.js
+++ app.js
@@ -3,18 +3,18 @@
  */
 function convertToFahrenheight(a) {
     "use strict";
-    a = document.getElementById('fahrenheight').value;
+    a = document.getElementById('temperature').value;
     a = a * 1.8;
     a = a + 32;
-    var answer = document.getElementById('displayFahrenheight');
+    var answer = document.getElementById('display');
     answer.innerHTML = a + 'F';
 }
 
 function convertToCelcius(a) {
     "use strict";
-    a = document.getElementById('celcius').value;
+    a = document.getElementById('temperature').value;
     a = a - 32.0;
     a = a * 0.55;
-    var answer = document.getElementById('displayCelcius');
+    var answer = document.getElementById('display');
     answer.innerHTML = a + 'C';
 }
