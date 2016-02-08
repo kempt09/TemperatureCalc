7628f3102ec3caca2561bffce86f000c3d9ea000
christopher <kempt09@gmail.com>
2016-02-08 14:55:43 -0500

    Refactored code

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 /**
  * Created by Christopher on 2/4/16.
  */
-function convertToFahrenheight(a) {
-    "use strict";
-    a = document.getElementById('temperature').value;
-    a = a * 1.8;
-    a = a + 32;
-    var answer = document.getElementById('display');
-    answer.innerHTML = a + 'F';
-}
 
-function convertToCelcius(a) {
-    "use strict";
-    a = document.getElementById('temperature').value;
-    a = a - 32.0;
-    a = a * 0.55;
-    var answer = document.getElementById('display');
-    answer.innerHTML = a + 'C';
+var utils = {
+
+    convertToFahrenheight: function (a) {
+        "use strict";
+        a = document.getElementById('temperature').value;
+        a = a * 1.8;
+        a = a + 32;
+        var answer = document.getElementById('display');
+        answer.innerHTML = a + 'F';
+    },
+    convertToCelcius: function (a) {
+        "use strict";
+        a = document.getElementById('temperature').value;
+        a = a - 32.0;
+        a = a * 0.55;
+        var answer = document.getElementById('display');
+        answer.innerHTML = a + 'C';
+    }
+    
 }
